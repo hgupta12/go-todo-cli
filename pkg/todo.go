9ecf639dfd1dd7893d90204640013e9b084df18a
harshit <harshitgupta1406@gmail.com>
2023-02-12 01:49:56 +0530

    data now stored in mysql database

--- pkg/todo.go
+++ pkg/todo.go
@@ -1,24 +1,28 @@
 package todo
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
-	"os"
-	"strconv"
-	"strings"
+	"net/http"
+	"net/url"
 	"time"
 )
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
 var fileName = "todos.txt"
 
+type Response struct{
+	Status string
+	Code string
+	Todos []ToDo `json:"todos"`
+}
 type ToDo struct{
-	id string
-	todo string
-	completed bool
+	Id string `json:"id"`
+	Todo string `json:"todo"`
+	Completed bool `json:"completed"`
 }
 
 func init(){
@@ -34,43 +38,31 @@ func generateId() string{
 }
 
 func getAllTodos() []ToDo{
-	f, err:= os.Open(fileName)
-
-	if err!=nil{
+	res,err := http.Get("http://localhost:8080/todos")
+	if err !=nil {
 		log.Fatal(err)
 	}
 
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	list := []ToDo{}
-	for scanner.Scan(){
-		text := strings.Split(scanner.Text(),",")
-		completed, _ := strconv.ParseBool(text[2])
-		todo := ToDo{text[0],text[1], completed}
-		list = append(list,todo)
-	}
-
-	if err := scanner.Err(); err != nil{
+	defer res.Body.Close()
+	body,err := ioutil.ReadAll(res.Body)
+	if err !=nil{
 		log.Fatal(err)
 	}
-
-	return list
+	var response Response
+	json.Unmarshal(body,&response)
+	return response.Todos
 }
 
 func AddToDo(todos []string){
-	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY,0644)
-	
-	if err!= nil{
-		log.Fatal(err)
-	}
-	defer f.Close()
 	for _,str := range todos{
 		id := generateId()
-		todo := ToDo{
-			id,str,false,
+		todo := url.Values{
+			"id": {id},
+			"todo":{str},
 		}
-		if _,err := f.WriteString(fmt.Sprintf("%s,%s,%v\n",todo.id,todo.todo,todo.completed)); err != nil{
+		fmt.Println(todo)
+		_,err := http.PostForm("http://localhost:8080/todos",todo)
+		if err != nil{
 			log.Fatal(err)
 		}
 	}
@@ -79,7 +71,7 @@ func ShowToDoList(all bool){
 	list := getAllTodos()
 	var output bool
 	for _,item := range list{
-		if all || !item.completed{
+		if all || !item.Completed{
 			fmt.Printf("%v\n",item)
 			output = true
 		}
@@ -91,33 +83,33 @@ func ShowToDoList(all bool){
 }
 
 func DeleteToDo(id string){
-	list := getAllTodos()
-	var data string
-	for _,item := range list{
-		if item.id != id{
-			data = data + fmt.Sprintf("%s,%s,%v\n",item.id,item.todo,item.completed)
-		}
-	}
+	url := fmt.Sprintf("http://localhost:8080/todos/%s",id)
+	client := &http.Client{}
 
-	if err:=ioutil.WriteFile(fileName,[]byte(data),0644); err!=nil{
+	req,err := http.NewRequest(http.MethodDelete,url,nil)
+	if err!=nil{
 		log.Fatal(err)
 	}
+	if _,err :=client.Do(req); err != nil{
+		log.Fatal(err)
+	}
+
 	fmt.Println("ToDo deleted!")
 }
 
 func MarkToDo(id string,f bool){
-	list := getAllTodos()
-	var data string
-	for _,item := range list{
-		if item.id == id{
-			data = data + fmt.Sprintf("%s,%s,%v\n",item.id,item.todo,!f)
-		}else{
-			data = data + fmt.Sprintf("%s,%s,%v\n",item.id,item.todo,item.completed)
-		}
-	}
+	url := fmt.Sprintf("http://localhost:8080/todos/%s?value=%v",id,!f)
 
-	if err:=ioutil.WriteFile(fileName,[]byte(data),0644); err!=nil{
+	client := &http.Client{}
+
+	req,err := http.NewRequest(http.MethodPatch, url,nil)
+	if err!=nil{
 		log.Fatal(err)
 	}
+
+	if _,err :=client.Do(req); err != nil{
+		log.Fatal(err)
+	}
+
 	fmt.Printf("ToDo Marked as %v!\n",!f)
 }
